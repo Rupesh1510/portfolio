56cd0b55356175972dc8f6f40491d2dfce108d62
Rupesh Kadam <94752269+Rupesh1510@users.noreply.github.com>
2025-03-20 10:41:04 +0530

    initial commit

---
+++ user-data/data.js
@@ -0,0 +1,174 @@
+export const bio = [
+"👨‍💻 Hello, World! I'm Rupesh Kadam 👋",
+"🔥 Tech Addict | Embedded Systems Wizard | Open-Source Advocate",
+"If it's got code, circuits, and a challenge, count me in! I'm a self-confessed Electronics Enthusiast and Tech Savant who loves building cool stuff at the intersection of hardware and software. Whether it's writing firmware, designing PCBs, or optimizing code, I live for those moments of 'Eureka!' 🔧.",
+];
+
+export const skills = [
+"Languages: C, C++, Python, Verilog HDL, MATLAB, Assembly, Bash",
+"Boards & Chips: ESP32, STM32, Embedded System Design, Firmware Dev, Keil uVision",
+"Design & Simulation: PCB Design (Eagle), Circuit Simulation (LT Spice), CAD 3D Modeling (Fusion360)",
+"Prototyping: 3D Printing, Laser Cutting, Rapid Prototyping, Circuit Fabrication",
+"Other Tech: Git, Hardware Design, IoT Systems, Schematic Design",
+"Human Skills: Critical Thinking, Problem Solving, Teamwork, Time Travel (wait, I mean Time Management 😅)",
+];
+
+export const experience = [
+  {
+    title: "Whirlpool of India Ltd., ITTP Kharadi, Pune",
+    duration: "Jan 2023 - Present",
+    subtitle: "Specialist Software Engineer",
+    details: [
+      "UART Communication Implementation: Implemented efficient UART communication between two control boards using a timeslot-based (RTOS) layered architecture, optimizing data transfer and system performance",
+      "Reduced Standby Power: Successfully proposed a solution that decreased standby power in a small domestic appliance by 66% (from 0.9W to 0.3W), meeting stringent IEC standby requirement standards",
+      "Developed and implemented an automated testing framework using FreeRTOS on a microcontroller to streamline the manual testing process for a small domestic appliance control board. Reduced manual testing time by 90% (from 20 hours to 2 hours). Improved test coverage and accuracy, leading to higher product quality.",
+      "Proficient in embedded software development for ARM, AVR, and Renesas microcontrollers. Experienced in 32-bit architectures and communication protocols such as I2C, SPI, and UART",
+    ],
+    tags: ["Embedded C/C++", "Embedded Systems", "Microcontrollers", "Automation", "Communications Protocols", "ARM Cortex M",],
+    icon: "truck ",
+  },
+  {
+    title: "Tata Centre for Technology & Design, IIT Bombay, Mumbai",
+    duration: "July 2022 – Dec 2022",
+    subtitle: "Electronics Engineer Intern",
+    details: [
+      "Pulmonary Function Testing Device: Spearheaded research and proof-of-concept development for a PFT device to diagnose COPD. Conducted rigorous experiments, including pressure and mass-flow measurements, using advanced electronic sensors. Engineered prototype designs and 3D-printed models. Conducted an extensive literature review of over 50 research papers and scientific articles.",
+      "Portable Infusion Pump: Designed and developed an embedded system for a portable infusion pump capable of precisely controlling IV fluid flow rates as low as 5ml/hr, catering to the critical needs of chemotherapy patients at Tata Memorial Hospital, Mumbai.",
+      "Technical Expertise: Demonstrated proficiency in digital signal processing, sensor data acquisition, embedded system design, and task automation using Python and MATLAB.",
+      "Rapid Prototyping: Utilized cutting-edge rapid prototyping technologies, including 3D printing, laser cutting, PCB fabrication, and machining tools, to expedite the development of proof-of-concept models.",
+    ],
+    tags: [
+      "Python",
+      "MATLAB", 
+      "Digital Signal Processing",
+      "Algorithms",
+      "Version Control",
+      "Simulation",
+      "Shell Scripting",
+      "Rapid Prototyping",
+      "PCB Design",
+    ],
+    icon: "heartbeat",
+  },
+  {
+    title: "MicroEmbedded Technologies",
+    duration: "May 2019 - Jun 2019",
+    subtitle: "Embedded Systems Trainee",
+    details: [
+      "Six weeks of Industrial Training on Embedded Systems (8051 Microcontroller Development Boards), Components assembly, and PCB testing",
+      "Developed QR code integration for wallet money loading, streamlining retailer workflows and reducing errors by 25 percent.",
+      "Integrated Clevertap analytics, Freshdesk ticketing, and chatbot services, enhancing user support and engagement.",
+    ],
+    tags: ["8051 MCU", "Verilog HDL", "Assembly Language", "Hardware Design", "System Testing"],
+    icon: "qrcode",
+  },
+  {
+    title: "Dharavi Diary: Slum & Rural Innovation Project",
+    duration: "Apr 2016 - Jun 2018",
+    subtitle: "Program Facilitator - Science & Electronics",
+    details: [
+      "Program Facilitator for Hands-on Learning STEAM workshops at Dharavi Diary: Slum and Rural Innovation Project. Worked with underprivileged children and a multidisciplinary team",
+      "Taught Science and Mathematics to 8th to 10th grade students",
+      "Organized coding workshops, health camps, street plays, beach cleaning campaign and a sci-tech festival called STEAM Fest 2018",
+      "Presented an interactive art installation, Around & Within the World at Kala Ghoda Art Festival 2017. It was created using recyclable materials collected from Dharavi: the recycle hub of Mumbai. Using IR sensors and LEDs it was made interactive to highlight the problems of the world around and how the world was before humans",
+    ],
+    tags: ["Design Thinking", "Electronics", "Programming", "STEM", "Non-Profit"],
+    icon: "group",
+  },
+];
+
+export const trekking = [
+  "<strong>Kheerganga Trek</strong>, Himachal Pradesh (9,711 feet)",
+  "<strong>Triund Trek</strong>, Himachal Pradesh (9,350 feet)",
+  "<strong>Kedarkantha Trek</strong>, Uttarakhand (12,500 feet)",
+  "<strong>Jalori Pass Trek</strong>, Himachal Pradesh (10,800 feet)",
+  "<strong>Vaishno Devi Trek</strong>, Jammu & Kashmir (5,200 feet)",
+];
+
+export const education = [
+  {
+    title: "B.Tech. in Electronics & Telecommunication Engineering",
+    duration: "2020 - 2023",
+    subtitle: "Vishwakarma Institute of Technology, Pune",
+    details: ["CGPA: 8.99/10.00"],
+    tags: [
+      "Data Structures & Algorithms",
+      "Operating Systems",
+      "Embedded Systems",
+      "Computer Networks",
+      "VLSI Design",
+      "Cloud Computing",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "Diploma in Electronics & Telecommunication Engineering",
+    duration: "2020",
+    subtitle: "Government Polytechnic, Awasari (kh.), Pune",
+    details: ["Aggregate Percentage: 90.24%"],
+    tags: ["Analog & Digital Electronics", "Embedded Systems", "Computer Networks", "Mobile Networks"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+  //   {
+  //     label: "Dev Profiles",
+  //     data: [
+  //       {
+  //         text: "Stackoverflow",
+  //         link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+  //       },
+  //       {
+  //         text: "GitHub",
+  //         link: "https://github.com/vinaysomawat",
+  //       },
+  //       {
+  //         text: "LeetCode",
+  //         link: "https://leetcode.com/somawatvinay/",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     label: "Resources",
+  //     data: [
+  //       {
+  //         text: "Enable Dark/Light Mode",
+  //         func: "enableDarkMode()",
+  //       },
+  //       {
+  //         text: "Print this page",
+  //         func: "window.print()",
+  //       },
+  //       {
+  //         text: "Clone this page",
+  //         link: "https://github.com/vinaysomawat/vinaysomawat.github.io",
+  //       },
+  //     ],
+  //   },
+  {
+    label: "Links",
+    data: [
+      //   {
+      //     text: "Linkedin",
+      //     link: "https://www.linkedin.com/in/vinaysomawat/",
+      //   },
+      //   {
+      //     text: "Twitter",
+      //     link: "https://twitter.com/thesigmakid",
+      //   },
+      {
+        text: "Buy me a coffee",
+        link: "https://www.linkedin.com/in/rupesh1510/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/Rupesh1510",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: ["Made with &hearts; by Rupesh Kadam"],
+  },
+];
